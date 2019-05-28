78ba6992b9172c35596e33495883ad7474ae6c46
John Omotani <john.omotani@ukaea.uk>
2019-05-28 10:21:09 +0100

    Test for saving/loading eqdsk files

---
+++ test_suite/test_torpex.py
@@ -0,0 +1,40 @@
+import pytest
+from equilibrium import Equilibrium
+from torpex_gridgen import *
+from test_utils import *
+
+class TestTORPEX:
+
+    @pytest.fixture
+    def equilib(self):
+        equilib = Equilibrium()
+        equilib.psi = lambda R, Z: (R - .98)**2 - .7*(Z - .04)**2
+        equilib.psi_sep = [0.]
+        equilib.fpol = lambda psi: 1.
+        equilib.TORPEX_wall = lambda theta: Point2D( .65 + .85*numpy.cos(theta),
+                .8 + .85*numpy.sin(theta))
+
+        return equilib
+
+    def test_Eqdsk(self, equilib):
+        testfile = 'test_torpex_gridgen.g'
+
+        nR = 20
+        Rmin = -1.
+        Rmax = 1.4
+        nZ = 40
+        Zmin = -.4
+        Zmax = 2.
+
+        createEqdsk(equilib, nR=nR, Rmin=Rmin, Rmax=Rmax, nZ=nZ, Zmin=Zmin, Zmax=Zmax,
+                filename=testfile)
+
+        grid_equilib = TORPEXMagneticField({'gfile':testfile}, {})
+
+        R = numpy.linspace(Rmin, Rmax, nR)[numpy.newaxis, :]
+        Z = numpy.linspace(Zmin, Zmax, nZ)[:, numpy.newaxis]
+
+        assert equilib.psi(R, Z) == pytest.approx(grid_equilib.psi(R,Z), rel=1.e-10)
+
+        import os
+        os.remove(testfile)

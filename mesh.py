49723b65bb354a91aa6791168ebdc172ecbe5140
John Omotani <john.omotani@ukaea.uk>
2019-06-01 22:37:49 +0100

    Fix number of points in non-orthogonal regridding

--- mesh.py
+++ mesh.py
@@ -287,8 +287,8 @@ class MeshRegion:
 
         if not meshParent.orthogonal:
             for contour in self.contours:
-                contour.getRegridded(self.ny, sfunc=self.equilibriumRegion.sfunc,
-                        width=regrid_width,
+                contour = contour.getRegridded(2*self.ny + 1,
+                        sfunc=self.equilibriumRegion.sfunc, width=regrid_width,
                         extend_lower=self.equilibriumRegion.extend_lower,
                         extend_upper=self.equilibriumRegion.extend_upper)
 

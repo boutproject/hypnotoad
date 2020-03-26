f2233325358e4a2de9e30ab36c976183af968bff
Ben Dudson <bd512@york.ac.uk>
2020-03-26 18:24:00 +0000

    Adding options for grid generation
    Normalised psi can be used, and is converted to psi.
    The psi values can be overridden by user options if needed.

--- tokamak.py
+++ tokamak.py
@@ -5,9 +5,12 @@
 import numpy as np
 from scipy import interpolate
 import warnings
+from collections import OrderedDict
 
 from .equilibrium import Equilibrium  # Base class 
 from .equilibrium import Point2D 
+from .equilibrium import setDefault # For options 
+from .hypnotoad_options import HypnotoadOptions
 
 from . import critical
 from . import polygons
@@ -29,7 +32,43 @@ class TokamakEquilibrium(Equilibrium):
             taken to be connected
     """
 
-    def __init__(self, R1D, Z1D, psi2D, psi1D, fpol1D, wall=[]):
+    # Tokamak-specific options and default values
+    default_options = HypnotoadOptions.add(
+
+        # Radial grid cell numbers
+        nx_core = 5,   # Number of radial points in the core
+        nx_pf = None,  # Number of radial points in the PF region
+        nx_pf_lower = None,
+        nx_pf_upper = None,
+        nx_sol = 5, # Number of radial points in the SOL
+        
+        # Poloidal grid cell numbers
+        ny_inner_lower_divertor = 4,
+        ny_inner_sol = 4,
+        ny_inner_upper_divertor = 4,
+        ny_outer_upper_divertor = 4,
+        ny_outer_sol = 4,
+        ny_outer_lower_divertor = 4,
+
+        # Normalised poloidal flux ranges.
+        psinorm_core = 0.9,
+        psinorm_sol = 1.1,
+        psinorm_sol_inner = None,    # Default: psinorm_sol
+        psinorm_pf = None,           # Default: psinorm_core
+        psinorm_pf_lower = None,     # Default: psinorm_pf
+        psinorm_pf_upper = None,     # Default: psinorm_pf
+
+        # Poloidal flux ranges.
+        # These are the values which are used in the mesh generation
+        # The default values comes from the psinorm values, but the user
+        # can override these defaults.
+        psi_core = None,         # Default: psinorm_core
+        psi_sol = None,          # Default: psnorm_sol
+        psi_sol_inner = None,    # Default: psinorm_sol_inner
+        psi_pf_lower = None,     # Default: psinorm_pf_lower
+        psi_pf_upper = None)     # Default: psinorm_pf_upper
+    
+    def __init__(self, R1D, Z1D, psi2D, psi1D, fpol1D, wall=[], psi_axis=None):
         # Create a 2D spline interpolation for psi
         self.psi_func = interpolate.RectBivariateSpline(R1D, Z1D, psi2D)
 
@@ -49,7 +88,11 @@ class TokamakEquilibrium(Equilibrium):
         opoints, xpoints = critical.find_critical(R2D, Z2D, psi2D)
         if len(opoints) == 0:
             warnings.warn("No O-points found in TokamakEquilibrium input")
-
+        else:
+            if psi_axis is None:
+                psi_axis = opoints[0][2] # Psi on magnetic axis
+        self.psi_axis = psi_axis
+        
         if len(xpoints) == 0:
             warnings.warn("No X-points found in TokamakEquilibrium input")
         
@@ -68,6 +111,89 @@ class TokamakEquilibrium(Equilibrium):
         self.wall = [Point2D(r,z) for r,z in wall]
         
 
+    def optionsTableStr(self):
+        """Return a string containing a table of options set"""
+        formatstring = '{:<50}|  {:<30}\n'
+
+        # Header
+        result = ('\nOptions\n=======\n'
+                  + formatstring.format('Name', 'Value')
+                  + '-'*80
+                  + "\n")
+
+        # Row for each value
+        for name, value in sorted(self.user_options.items()):
+            valuestring = str(value)
+            if value == self.default_options[name]:
+                valuestring += '\t(default)'
+            result += formatstring.format(name, valuestring)
+        return result
+        
+    def makeRegions(self, **kwargs):
+        assert 0 < len(self.x_points) <= 2
+        assert self.psi_axis is not None
+        
+        self.user_options = self.default_options.copy()
+        self.user_options = self.user_options.push(kwargs)
+
+        # Radial grid cells in PF regions
+        setDefault(self.user_options, 'nx_pf', self.user_options.nx_core)
+        setDefault(self.user_options, 'nx_pf_lower', self.user_options.nx_pf)
+        setDefault(self.user_options, 'nx_pf_upper', self.user_options.nx_pf)
+
+        # Normalised psi values
+        setDefault(self.user_options, 'psinorm_pf', self.user_options.psinorm_core)
+        setDefault(self.user_options, 'psinorm_pf_lower', self.user_options.psinorm_pf)
+        setDefault(self.user_options, 'psinorm_pf_upper', self.user_options.psinorm_pf)
+        
+        setDefault(self.user_options, 'psinorm_sol_inner', self.user_options.psinorm_sol)
+        
+        # psi values
+        def psinorm_to_psi(psinorm):
+            if psinorm is None:
+                return None
+            return self.psi_axis + psinorm * (self.psi_sep[0] - self.psi_axis)
+        
+        setDefault(self.user_options, 'psi_core',
+                   psinorm_to_psi(self.user_options.psinorm_core))
+        setDefault(self.user_options, 'psi_sol',
+                   psinorm_to_psi(self.user_options.psinorm_sol))
+        setDefault(self.user_options, 'psi_sol_inner',
+                   psinorm_to_psi(self.user_options.psinorm_sol_inner))
+        setDefault(self.user_options, 'psi_pf_lower',
+                   psinorm_to_psi(self.user_options.psinorm_pf_lower))
+        setDefault(self.user_options, 'psi_pf_upper',
+                   psinorm_to_psi(self.user_options.psinorm_pf_upper))
+        
+        # Print the table of options
+        print(self.optionsTableStr())
+        
+        self.regions = OrderedDict()
+        
+        if len(self.x_points) == 1:
+            # Single null. Could be lower or upper
+
+            # Lower Single Null
+            # Regions ordered clockwise, starting lower inner divertor
+            legnames = ['inner_lower_divertor', 'sol', 'outer_lower_divertor']
+            kinds = ['wall.X', 'X.X', 'X.wall']
+
+            # Average grid spacing
+            dpsidi_sol = (self.user_options.psi_sol - self.psi_sep[0]) / self.user_options.nx_sol
+            dpsidi_core = (self.psi_sep[0] - self.user_options.psi_core) / self.user_options.nx_core
+            dpsidi_pf = (self.psi_sep[0] - self.user_options.psi_pf_lower) / self.user_options.nx_pf_lower
+
+            
+        else:
+            # Double null
+            
+            # Regions ordered clockwise, starting lower inner divertor
+            legnames = ['inner_lower_divertor', 'inner_sol', 'inner_upper_divertor',
+                        'outer_upper_divertor', 'outer_sol', 'outer_lower_divertor']
+            kinds = ['wall.X', 'X.X', 'X.wall',
+                     'wall.X', 'X.X', 'X.wall']
+            
+        
 
         
     def psi(self, R, Z):
@@ -143,4 +269,30 @@ def read_geqdsk(filehandle):
                               data["fpol"], # fpol1D
                               wall=wall)
 
+    
+def example():
+    nx = 65
+    ny = 65
+    
+    r1d = np.linspace(1.2, 1.8, nx)
+    z1d = np.linspace(-0.5, 0.5, ny)
+    r2d, z2d = np.meshgrid(r1d, z1d, indexing='ij')
+
+    r0 = 1.5
+    z0 = -0.3
+
+    # This has two O-points, and one x-point at (r0, z0)
+    def psi_func(R,Z):
+        return np.exp(-((R - r0)**2 + (Z - z0 - 0.3)**2)/0.3**2) + np.exp(-((R - r0)**2 + (Z - z0 + 0.3)**2)/0.3**2)
+    
+    eq = TokamakEquilibrium(r1d, z1d, psi_func(r2d, z2d),
+                            [], []) # psi1d, fpol
+
+    import matplotlib.pyplot as plt
+
+    eq.plotPotential()
+    plt.plot(*eq.x_points[0], 'rx')
+    plt.show()
+    
+    eq.makeRegions()
     
---
+++ tokamak_example.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+from hypnotoad2 import tokamak
+
+tokamak.example()

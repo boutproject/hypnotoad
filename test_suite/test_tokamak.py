Fix issues in f_R and f_Z calculation.
Some confusion about whether |Grad psi| or |Grad psi|^2 should be
used. With consistent definition the cubic spline differentiation
can be used.

test_suite/test_tokamak.py
@@ -8,8 +8,8 @@ def test_tokamak_interpolations():
     """Test interpolations and derivatives"""
     
     # Define 2D (R,Z) grid
-    r1d = np.linspace(1.0, 2.0, 65)
-    z1d = np.linspace(-1.0, 1.0, 65)
+    r1d = np.linspace(1.0, 2.0, 129)
+    z1d = np.linspace(-1.0, 1.0, 129)
     r2d, z2d = np.meshgrid(r1d, z1d, indexing='ij')
 
     # A poloidal flux function
@@ -58,12 +58,12 @@ def test_tokamak_interpolations():
         assert np.isclose(eq.Bp_Z(r, z), -dpsi_dr(r, z)/r, atol=1e-3)
 
         # vector Grad(psi)/|Grad(psi)|**2
-        assert np.isclose(eq.f_R(r, z), dpsi_dr(r, z) / np.sqrt(dpsi_dr(r, z)**2 +
-                                                                dpsi_dz(r, z)**2),
-                          atol=1e-3)
-        assert np.isclose(eq.f_Z(r, z), dpsi_dz(r, z) / np.sqrt(dpsi_dr(r, z)**2 +
-                                                                dpsi_dz(r, z)**2),
-                          atol=1e-3)
+        assert np.isclose(eq.f_R(r, z), dpsi_dr(r, z) /(dpsi_dr(r, z)**2 +
+                                                        dpsi_dz(r, z)**2),
+                          rtol=1e-3)
+        assert np.isclose(eq.f_Z(r, z), dpsi_dz(r, z) / (dpsi_dr(r, z)**2 +
+                                                         dpsi_dz(r, z)**2),
+                          rtol=1e-3)
         
 
 def test_read_geqdsk():
@@ -151,12 +151,12 @@ def test_read_geqdsk():
         assert np.isclose(eq.Bp_Z(r, z), -dpsi_dr(r, z)/r, atol=1e-3)
 
         # vector Grad(psi)/|Grad(psi)|**2
-        assert np.isclose(eq.f_R(r, z), dpsi_dr(r, z) / np.sqrt(dpsi_dr(r, z)**2 +
-                                                                dpsi_dz(r, z)**2),
-                          atol=1e-3)
-        assert np.isclose(eq.f_Z(r, z), dpsi_dz(r, z) / np.sqrt(dpsi_dr(r, z)**2 +
-                                                                dpsi_dz(r, z)**2),
-                          atol=1e-3)
+        assert np.isclose(eq.f_R(r, z), dpsi_dr(r, z) / (dpsi_dr(r, z)**2 +
+                                                         dpsi_dz(r, z)**2),
+                          rtol=1e-3)
+        assert np.isclose(eq.f_Z(r, z), dpsi_dz(r, z) / (dpsi_dr(r, z)**2 +
+                                                         dpsi_dz(r, z)**2),
+                          rtol=1e-3)
 
 def test_bounding():
     nx = 65

tokamak.py
@@ -1084,32 +1084,16 @@ class TokamakEquilibrium(Equilibrium):
     @handleMultiLocationArray
     def f_R(self, R, Z):
         """returns the R component of the vector Grad(psi)/|Grad(psi)|**2."""
-        # Note: The following lines should work, but
-        # give wrong answers. Instead a simple method is used for now.
-        #dpsidR = self.psi_func(R, Z, dx=1, grid=False)
-        #dpsidZ = self.psi_func(R, Z, dy=1, grid=False)
-        #return dpsidR / np.sqrt(dpsidR**2 + dpsidZ**2)
-        eps = 1e-10
-        psi0 = self.psi(R,Z)
-        dpsidr = (self.psi(R + eps, Z) - psi0) / eps
-        dpsidz = (self.psi(R, Z + eps) - psi0) / eps
-        norm = 1. / (dpsidr**2 + dpsidz**2) # Common factor
-        return dpsidr * norm
+        dpsidR = self.psi_func(R, Z, dx=1, grid=False)
+        dpsidZ = self.psi_func(R, Z, dy=1, grid=False)
+        return dpsidR / (dpsidR**2 + dpsidZ**2)
 
     @handleMultiLocationArray
     def f_Z(self, R, Z):
         """returns the Z component of the vector Grad(psi)/|Grad(psi)|**2."""
-        # Note: The following lines should work, but
-        # give wrong answers. Instead a simple method is used for now.
-        #dpsidR = self.psi_func(R, Z, dx=1, grid=False)
-        #dpsidZ = self.psi_func(R, Z, dy=1, grid=False)
-        #return dpsidZ / np.sqrt(dpsidR**2 + dpsidZ**2)
-        eps = 1e-10
-        psi0 = self.psi(R,Z)
-        dpsidr = (self.psi(R + eps, Z) - psi0) / eps
-        dpsidz = (self.psi(R, Z + eps) - psi0) / eps
-        norm = 1. / (dpsidr**2 + dpsidz**2) # Common factor
-        return dpsidz * norm
+        dpsidR = self.psi_func(R, Z, dx=1, grid=False)
+        dpsidZ = self.psi_func(R, Z, dy=1, grid=False)
+        return dpsidZ / (dpsidR**2 + dpsidZ**2)
 
     @handleMultiLocationArray
     def Bp_R(self, R, Z):
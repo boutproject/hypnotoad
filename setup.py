e14712e54a94dfdbfe1a0488a9e52810c615c7a9
John Omotani <john.omotani@ukaea.uk>
2020-04-21 16:12:40 +0100

    Different gui-* installation options for different Qt wrappers
    Instead of "pip install hypnotoad[gui]", have different versions for
    pyside2 or PyQt5 (which the gui needs one but not both of, and in
    principle we could use pyside or PyQt4 as well, if there was demand for
    them). So now do "pip install hypnotoad[gui-pyside2]" to bring in
    pyside2 or "pip install hypnotoad[gui-PyQt5]" to bring in PyQt5>=5.12.

--- setup.py
+++ setup.py
@@ -35,14 +35,20 @@ setuptools.setup(
         "pyparsing~=2.4",
         "PyYAML~=5.1",
         "scipy~=1.4",
+        "Qt.py~=1.2",
     ],
-    extras_require={"gui": ["Qt.py~=1.2", "pyside2~=5.14"]},
+    extras_require={
+        "gui-pyside2": ["pyside2~=5.14"],
+        "gui-PyQt5": ["PyQt5~=5.12"],
+    },
     python_requires=">=3.6",
     entry_points={
         "console_scripts": [
             "hypnotoad2_geqdsk = hypnotoad2.scripts.hypnotoad2_geqdsk:main",
             "hypnotoad2_torpex = hypnotoad2.scripts.hypnotoad2_torpex:main",
         ],
-        "gui_scripts": ["hypnotoad2-gui = hypnotoad2.gui:main [gui]"],
+        "gui_scripts": [
+            "hypnotoad2-gui = hypnotoad2.gui:main",
+        ],
     },
 )

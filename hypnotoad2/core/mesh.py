80091a2d19f9c93e58f495c22377595f30ba86ee
John Omotani <john.omotani@ukaea.uk>
2020-04-22 14:46:43 +0100

    Auto-detect whether hypnotoad2 is in git repo or installed
    Removes need for __init__.__release__ flag.
    
    Also always save version number, just save git hash in addition, if
    hypnotoad2 is in a git repo.

--- hypnotoad2/core/mesh.py
+++ hypnotoad2/core/mesh.py
@@ -29,8 +29,10 @@ import numpy
 from scipy.integrate import solve_ivp
 
 from boututils.boutarray import BoutArray
+from boututils.run_wrapper import shell, shell_safe
 
 from .equilibrium import calc_distance, Point2D
+from ..__version__ import __version__
 
 
 class MultiLocationArray(numpy.lib.mixins.NDArrayOperatorsMixin):
@@ -1812,31 +1814,43 @@ class Mesh:
 
         self.equilibrium = equilibrium
 
-        # Get release version number for releases or current git-commit hash of
-        # Hypnotoad2 for git-clone'd versions for version-tracking
-        from ..__version__ import __release__
+        self.version = __version__
+        self.git_hash = None
+        self.git_diff = None
 
-        if __release__:
-            from ..__version__ import __version__
+        # check if git exists
+        retval, git_version = shell("git --version")
+        if retval == 0:
+            # git exists
 
-            self.version_identifier = __version__
-            self.git_diff = None
-        else:
-            from boututils.run_wrapper import shell_safe
             from pathlib import Path
+            from hypnotoad2.__init__ import __file__ as hypnotoad2_init_file
 
-            hypnotoad_path = str(Path(__file__).parent)
-            retval, self.version_identifier = shell_safe(
-                "cd "
-                + hypnotoad_path
-                + '&& git describe --always --abbrev=0 --dirty --match "NOT A TAG"',
-                pipe=True,
+            hypnotoad2_path = Path(hypnotoad2_init_file).parent
+
+            # check if hypnotoad is in it's own git repo. hypnotoad2.__init__.py
+            # should be in the hypnotoad2/ subdirectory of the git repo if it is.
+            # So the parent directory of hypnotoad2_path should contain a '.git'
+            # directory if hypnotoad is in a git repo
+            if hypnotoad2_path.parent.joinpath(".git").is_dir():
+                retval, self.git_hash = shell_safe(
+                    "cd "
+                    + str(hypnotoad2_path)
+                    + '&& git describe --always --abbrev=0 --dirty --match "NOT A TAG"',
+                    pipe=True,
+                )
+                self.git_hash = self.git_hash.strip()
+                retval, self.git_diff = shell_safe(
+                    "cd " + str(hypnotoad2_path) + "&& git diff", pipe=True
+                )
+                self.git_diff = self.git_diff.strip()
+        elif retval == 127:
+            # git not installed
+            pass
+        else:
+            raise RuntimeError(
+                "'git --version' failed with {retval}. Output was {git_version}"
             )
-            self.version_identifier = self.version_identifier.strip()
-            retval, self.git_diff = shell_safe(
-                "cd " + hypnotoad_path + "&& git diff", pipe=True
-            )
-            self.git_diff = self.git_diff.strip()
 
         # Generate MeshRegion object for each section of the mesh
         self.regions = {}
@@ -2430,7 +2444,9 @@ class BoutMesh(Mesh):
                 f.write("parallel_transform", "identity")
 
             f.write("hypnotoad_inputs", self.equilibrium._getOptionsAsString())
-            f.write("hypnotoad_version", self.version_identifier)
+            f.write("hypnotoad_version", self.version)
+            if self.git_hash is not None:
+                f.write("hypnotoad_git_hash", self.git_hash)
             if self.git_diff is not None:
                 f.write("hypnotoad_git_diff", self.git_diff)
 

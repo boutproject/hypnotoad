3de5a905fd888d447a8f14b4c3120e791f37c1ea
John Omotani <john.omotani@ukaea.uk>
2021-06-01 13:52:58 +0100

    Fix used of closed_wallarray in Equilibrium.wallPosition()
    Caused errors for TORPEX case.

--- hypnotoad/core/equilibrium.py
+++ hypnotoad/core/equilibrium.py
@@ -4063,10 +4063,10 @@ class Equilibrium:
         except AttributeError:
             # wall interpolation functions not created yet
 
-            R = self.closed_wall_array[:, 0]
-            Z = self.closed_wall_array[:, 1]
+            R = self.closed_wallarray[:, 0]
+            Z = self.closed_wallarray[:, 1]
 
-            wallfraction = numpy.linspace(0.0, 1.0, len(self.wall))
+            wallfraction = numpy.linspace(0.0, 1.0, len(R))
 
             self.wallRInterp = interp1d(
                 wallfraction, R, kind="linear", assume_sorted=True

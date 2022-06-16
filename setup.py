1c661e15344249ecb7950d8bae6d30bbc251c36b
John Omotani <john.omotani@ukaea.uk>
2022-06-16 09:58:02 +0100

    Exclude dill-0.3.5
    dill 0.3.5 has a bug that breaks parallelisation, but should be fixed in
    0.3.6. For now, just exclude version 0.3.5.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     ],
     install_requires=[
         "boututils~=0.1.7",
-        "dill~=0.3",
+        "dill~=0.3,!=0.3.5,!=0.3.5.1",
         "func_timeout~=4.3",
         "matplotlib~=3.2",
         "netCDF4~=1.5",

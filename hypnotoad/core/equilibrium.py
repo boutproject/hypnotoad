fce913504e4cc00f5858aa582650cbb031f4262a
John Omotani <john.omotani@ukaea.uk>
2022-10-28 10:17:04 +0100

    Also suggest increasing spacing when gradient goes negative

--- hypnotoad/core/equilibrium.py
+++ hypnotoad/core/equilibrium.py
@@ -3424,8 +3424,9 @@ class EquilibriumRegion(PsiContour):
             # upper boundary:
             if a / (2.0 * numpy.sqrt(N / N_norm)) + d + 2.0 * e * N / N_norm <= 0.0:
                 raise ValueError(
-                    "gradient at end should be positive. Try setting "
-                    "poloidal_spacing_method to e.g. 'monotonic'"
+                    "gradient at end should be positive. Try increasing the spacing "
+                    "parameter at the other end, or setting poloidal_spacing_method to "
+                    "e.g. 'monotonic'"
                 )
 
             def upper_extrap(i):

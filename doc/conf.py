d49897819a484e2ad648e06159b33194b27db334
John Omotani <john.omotani@ukaea.uk>
2022-12-30 23:00:00 +0000

    Setup Sphinx documentation
    Create skeleton for Sphinx-generated docs.

---
+++ doc/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "hypnotoad"
+copyright = "2022, J.T. Omotani, B.D. Dudson and the hypnotoad team"
+author = "J.T. Omotani, B.D. Dudson and the hypnotoad team"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+sys.path.insert(0, os.path.abspath("."))
+
+# Are we running on readthedocs?
+on_rtd = os.environ.get("READTHEDOCS") == "True"
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+
+# templates_path = ['_templates']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+if on_rtd:
+    html_theme = "default"
+else:
+    html_theme = "alabaster"
+# html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,23 @@
+.. hypnotoad documentation master file, created by
+   sphinx-quickstart on Fri Dec 30 22:36:37 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to hypnotoad's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   quickstart
+   api
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
